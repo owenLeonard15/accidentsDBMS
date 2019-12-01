db47801ee85d46ef940d7e123993141d7a6e6042
Owen Leonard <oleonard15@gmail.com>
2019-12-01 15:45:42 -0600

    added heatmap functionality

--- js/index.js
+++ js/index.js
@@ -16,15 +16,18 @@ connection.connect()
 
 app.get('/', (req, res) => {
   let accidents = []
-  connection.query('SELECT * FROM accidents LIMIT 50', function (err, result) {
+  connection.query('SELECT * FROM accidents INNER JOIN location ON accidents.accident_id = location.accident_id LIMIT 200000', function (err, result) {
     if (err) throw err
-    for(let i = 0; i < 50; i++){
+    for(let i = 0; i < result.length; i++){
       accidents.push(result[i])
     }
     res.send(JSON.stringify({"status" : 200, "error": null, "accidents": accidents}))
   })
+  console.log(accidents.length);
   }
 )
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
+let accidents = [];
+
+// map center
+var myLatlng = new google.maps.LatLng(37.385208,-93.67223);
+// map options,
+var myOptions = {
+  zoom: 5,
+  center: myLatlng
+};
+// standard map
+map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+// heatmap layer
+heatmap = new HeatmapOverlay(map, 
+  {
+    // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+    "radius": 5,
+    "maxOpacity": 1, 
+    // scales the radius based on map zoom
+    "scaleRadius": false, 
+    // if set to false the heatmap uses the global maximum for colorization
+    // if activated: uses the data maximum within the current map boundaries 
+    //   (there will always be a red spot with useLocalExtremas true)
+    "useLocalExtrema": false,
+    // which field name in your data represents the latitude - default "lat"
+    latField: 'lat',
+    // which field name in your data represents the longitude - default "lng"
+    lngField: 'lng',
+    // which field name in your data represents the data value - default "value"
+    valueField: 'count'
+  }
+);
+
+
+
 document.getElementById('accident-btn').addEventListener('click', () => { 
     fetch('http://localhost:3000/', {
         method: 'GET',
@@ -7,10 +41,12 @@ document.getElementById('accident-btn').addEventListener('click', () => {
     })
     .then(res => res.json())
         .then(data => {
-            let accidents = data.accidents;
             let tableRef = document.getElementById('my-table');
-            for(let i = 0; i < data.accidents.length; i++){
+            let mapData = {max: 8, data: [] }
+            // this is for displaying the first 100 results in table format
+            for(let i = 0; i < 100; i++){
                 let newRow = tableRef.insertRow();
+                accidents = data.accidents;
 
                 let newAccidentIDCell = newRow.insertCell();
                 let newAccidentIDText  = document.createTextNode(accidents[i].accident_id);
@@ -20,6 +56,20 @@ document.getElementById('accident-btn').addEventListener('click', () => {
                 let newDescriptionText = document.createTextNode(accidents[i].description);
                 newAccidentDescriptionCell.appendChild(newDescriptionText);
             } 
+            // adds the data to an array, then passes the array all at once to the map via setData()
+            // using the addData function to add each point individually is significantly slower
+            for(let i = 0; i < data.accidents.length; i++){
+                let dataPoint = {
+                    lat: data.accidents[i].start_lat,
+                    lng: data.accidents[i].start_lng,
+                    count: 1
+                }
+                mapData.data.push(dataPoint);
+            }
+            heatmap.setData(mapData);
         })
     }
 )
+
+
+
